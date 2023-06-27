a5a08a39a002f713026c8d9ffdde0b9259c73f6c
william fevre <fevre.william@wanadoo.fr>
2023-06-27 15:59:38 +0200

    update

--- app/controllers/teacher/courses_controller.rb
+++ app/controllers/teacher/courses_controller.rb
@@ -1,2 +1,26 @@
 class Teacher::CoursesController < ApplicationController
+
+
+  def create
+    @pool = Pool.new(pool_params)
+    @pool.user = current_user
+    @pool.save
+    redirect_to root_path
+  end
+
+  def destroy
+    @pool = Pool.find(params[:id])
+    @pool.destroy
+    redirect_to owner_bookings_path
+  end
+
+  private
+
+  def set_course
+    @course = Course.find(params[:id])
+  end
+
+  def course_params
+    params.require(:course).permit(:name, :price, :description, :max_people, :address, :url_photo)
+  end
 end
