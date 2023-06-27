a7a7227a30b7d4e12ba5aad8f67faa59598a4e39
rayandev rungoo <rayan.rungoo@gmail.com>
2023-06-27 15:52:49 +0200

    front

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,5 +2,9 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
   def home
+    @courses = Course.all
+    # @teacher = User.find(1)
+    # @courses.teacher = @teacher
+    @teacher = Teacher.all
   end
 end
