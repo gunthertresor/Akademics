7fd0a13d73760c430f6fc396e0491c037508b381
william fevre <fevre.william@wanadoo.fr>
2023-07-04 00:17:05 +0200

    homev3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,18 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home ]
 
+#   def muniq(ary)
+#     ary.uniq
+#   end
+
   def home
-    # @courses = Course.all
-    @reviews = Review.where(rating: 5) # filtrer si meme nom de cour
-    # @reviews = Review.all # filtrer si meme nom de cour
-    # @courses = Course.joins(:reviews) #.where(rating: 5)
+    # @reviews = Review.all
+    @reviews = Review.where(rating: 5).uniq { |review| review[:course_id] }
+    # @counts = {}
+    # puts @reviews_count = Review.where(rating: 5).each_with_object(Hash.new(0)) { |name, hash| hash[name] += 1 }
+    # @reviews = Review.where(rating: 5)
+    # @reviews = mithun_sasidharan(@reviews2.course.name)
+    # @reviews.map{ |review| muniq(review.course.name) }
     # .limit(10)
     # @teacher = User.find(1)
     # @courses.teacher = @teacher
--- db/seeds.rb
+++ db/seeds.rb
@@ -19,15 +19,21 @@ susan = User.create(first_name: 'Susan', last_name: "Smith", email: "susan.smith
 bob = User.create(first_name: 'Bob', last_name: "Johnson", email: "bob.johnson@example.com", password: "rerere", phone_number: "345 678 9012", address: "789 Pine Lane", status: "student")
 linda = User.create(first_name: 'Linda', last_name: "Williams", email: "linda.williams@example.com", password: "rerere", phone_number: "456 789 0123", address: "012 Maple Drive", status: "student")
 amy = User.create(first_name: 'Amy', last_name: "Brown", email: "amy.brown@example.com", password: "rerere", phone_number: "567 890 1234", address: "345 Birch Boulevard", status: "student")
-george = User.create(first_name: 'George', last_name: "Davis", email: "george.davis@example.com", password: "rerere", phone_number: "678 901 2345", address: "678 Cedar Court", status: "teacher")
-megan = User.create(first_name: 'Megan', last_name: "Miller", email: "megan.miller@example.com", password: "rerere", phone_number: "789 012 3456", address: "901 Spruce Street", status: "teacher")
-steve = User.create(first_name: 'Steve', last_name: "Wilson", email: "steve.wilson@example.com", password: "rerere", phone_number: "890 123 4567", address: "234 Aspen Avenue", status: "teacher")
+george = User.create(first_name: 'George', last_name: "Davis", email: "george.davis@example.com", password: "rerere", phone_number: "678 901 2345", address: "678 Cedar Court", status: "teacher", url_avatar:"https://images.unsplash.com/photo-1599566150163-29194dcaad36?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80")
+megan = User.create(first_name: 'Megan', last_name: "Miller", email: "megan.miller@example.com", password: "rerere", phone_number: "789 012 3456", address: "901 Spruce Street", status: "teacher", url_avatar:"https://images.unsplash.com/photo-1586297135537-94bc9ba060aa?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1480&q=80")
+steve = User.create(first_name: 'Steve', last_name: "Wilson", email: "steve.wilson@example.com", password: "rerere", phone_number: "890 123 4567", address: "234 Aspen Avenue", status: "teacher", url_avatar:"https://images.unsplash.com/photo-1590086782957-93c06ef21604?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80")
+jim = User.create(first_name: 'jim', last_name: "gordon", email: "jim.gordon@example.com", password: "rerere", phone_number: "890 123 4567", address: "233 Aspen Avenue", status: "teacher", url_avatar:"https://images.unsplash.com/photo-1624561172888-ac93c696e10c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=989&q=80")
+joseph = User.create(first_name: 'joseph', last_name: "washington", email: "joseph.washington@example.com", password: "rerere", phone_number: "890 123 4567", address: "235 Aspen Avenue", status: "teacher", url_avatar:"https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=987&q=80")
 
 puts "-------------- create Courses --------------"
 course_george1 = Course.create(user_id: george.id, title: "Shakespearean literature", name: "Shakespeare's plays and sonnets", category: "literature", description: "A comprehensive study of Shakespeare's work.", content: "Shakespeare's literature is about drama, conflict...", price: 50, grade: 1, rating: 1, url_course:"https://www.youtube.com/watch?v=your_url")
-course_george2 = Course.create(user_id: george.id, title: "Modernist literature", name: "Exploring the modernists", category: "literature", description: "An exploration of modernist literature.", content: "Modernist literature is about the self, perception...", price: 40, grade: 2, rating: 5, url_course:"https://www.youtube.com/watch?v=your_url")
+course_george2 = Course.create(user_id: george.id, title: "Modernist art", name: "Exploring the modernists", category: "art", description: "An exploration of modernist literature.", content: "Modernist literature is about the self, perception...", price: 40, grade: 2, rating: 5, url_course:"https://www.youtube.com/watch?v=your_url")
 course_megan1 = Course.create(user_id: megan.id, title: "Calculus 101", name: "Introduction to calculus", category: "mathematics", description: "An introductory course in calculus.", content: "Calculus is about change, rates...", price: 100, grade: 5, rating: 4, url_course:"https://www.youtube.com/watch?v=your_url")
 course_steve1 = Course.create(user_id: steve.id, title: "Creative writing", name: "Writing creative fiction", category: "literature", description: "A course on writing creative fiction.", content: "Creative writing is about imagination, expression...", price: 200, grade: 4, rating: 5, url_course:"https://www.youtube.com/watch?v=your_url")
+course_jim1 = Course.create(user_id: jim.id, title: "learn chimie", name: "Learn modern chemistry", category: "chemistry", description: "How create new element", content: "introduction of modern chemistry...", price: 200, grade: 4, rating: 5, url_course:"https://www.youtube.com/watch?v=your_url")
+course_joseph1 = Course.create(user_id: joseph.id, title: "space geomety", name: "How does spacetime work?", category: "physics", description: "A course how space-time work on global scale", content: "We live in big...", price: 200, grade: 4, rating: 5, url_course:"https://www.youtube.com/watch?v=your_url")
+course_joseph2 = Course.create(user_id: joseph.id, title: "engineering", name: "The art of engineering", category: "engineering", description: "A course engineering", content: "engineering is about...", price: 200, grade: 4, rating: 5, url_course:"https://www.youtube.com/watch?v=your_url")
+course_joseph3 = Course.create(user_id: joseph.id, title: "robotic", name: "Robotic initiation", category: "robotic", description: "A course engineering", content: "engineering is about...", price: 200, grade: 4, rating: 5, url_course:"https://www.youtube.com/watch?v=your_url")
 
 puts "-------------- create Classe --------------"
 class1 = Classe.create(course_id: course_george1.id, name: "Shakespearean literature class", max_people: 20, class_date: Time.now + 3.days, start_date: Time.now + 3.hours, end_date: Time.now + 4.hours)
@@ -35,9 +41,9 @@ class3 = Classe.create(course_id: course_george1.id, name: "Shakespearean litera
 class3 = Classe.create(course_id: course_george1.id, name: "Shakespearean literature class", max_people: 20, class_date: Time.now + 5.days, start_date: Time.now + 3.hours, end_date: Time.now + 4.hours)
 class3 = Classe.create(course_id: course_george2.id, name: "Modernist literature class", max_people: 20, class_date: Time.now + 5.days, start_date: Time.now + 3.hours, end_date: Time.now + 4.hours)
 class2 = Classe.create(course_id: course_megan1.id, name: "Calculus 101 class", max_people: 20, class_date: Time.now + 10.days, start_date: Time.now + 3.hours, end_date: Time.now + 4.hours)
-class2 = Classe.create(course_id: course_megan1.id, name: "Calculus 101 class", max_people: 20, class_date: Time.now + 10.days, start_date: Time.now + 3.hours, end_date: Time.now + 4.hours)
-class2 = Classe.create(course_id: course_megan1.id, name: "Calculus 101 class", max_people: 20, class_date: Time.now + 10.days, start_date: Time.now + 3.hours, end_date: Time.now + 4.hours)
-class2 = Classe.create(course_id: course_steve1.id, name: "Creative writing class", max_people: 20, class_date: Time.now + 10.days, start_date: Time.now + 3.hours, end_date: Time.now + 4.hours)
+class4 = Classe.create(course_id: course_jim1.id, name: "learn chimie class", max_people: 20, class_date: Time.now + 10.days, start_date: Time.now + 3.hours, end_date: Time.now + 4.hours)
+class5 = Classe.create(course_id: course_jim1.id, name: "learn chimie class", max_people: 20, class_date: Time.now + 10.days, start_date: Time.now + 4.hours, end_date: Time.now + 5.hours)
+class6 = Classe.create(course_id: course_joseph1.id, name: "space geomety class", max_people: 20, class_date: Time.now + 10.days, start_date: Time.now + 3.hours, end_date: Time.now + 4.hours)
 
 puts "-------------- create Enrollment --------------"
 enrollment1 = Enrollment.create(user_id: john.id, classe_id: class1.id, cancelled: false)
@@ -56,5 +62,9 @@ Review.create(user_id: linda.id, course_id: course_steve1.id, rating: 4)
 Review.create(user_id: bob.id, course_id: course_george2.id, rating: 5)
 Review.create(user_id: susan.id, course_id: course_george2.id, rating: 1)
 Review.create(user_id: amy.id, course_id: course_megan1.id, rating: 5)
+Review.create(user_id: amy.id, course_id: course_joseph1.id, rating: 5)
+Review.create(user_id: linda.id, course_id: course_joseph2.id, rating: 5)
+Review.create(user_id: amy.id, course_id: course_jim1.id, rating: 5)
+Review.create(user_id: amy.id, course_id: course_joseph3.id, rating: 5)
 
 puts "-------------- create all ok --------------"
