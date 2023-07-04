c1737cd2a873307b983d4ea600279b8bd5cff8b5
william fevre <fevre.william@wanadoo.fr>
2023-07-04 22:30:53 +0200

    big update

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,6 +1,20 @@
+require "open-uri"
+require "nokogiri"
+
 class CoursesController < ApplicationController
     skip_before_action :authenticate_user!, only: [:index, :show]
     before_action :set_courses, only: [:show, :edit, :update, :destroy]
+
+    def scrap
+        url = "https://www.britannica.com/art/English-literature/Playwrights-after-Shakespeare"
+        html_file = URI.open(url).read
+        html_doc = Nokogiri::HTML.parse(html_file)
+        html_doc.search(".standard-card-new__article-title").each do |element|
+            # puts element.text.strip
+            element.attribute("href").value
+        end
+    end
+
 
     def index
         @courses = Course.all
@@ -9,6 +23,7 @@ class CoursesController < ApplicationController
     def show
         @enrollment = Enrollment.new
         @classes = @course.course_classes
+        @scrap = scrap()
         # @course.classe = Classe.
         # @course_classes = Course.Classe.new
     end
--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 class Users::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
-  # before_action :configure_account_update_params, only: [:update]
+#   before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_account_update_params, only: [:update ]
 
   # GET /resource/sign_up
   # def new
@@ -15,14 +15,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+  end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
   # DELETE /resource
   # def destroy
@@ -38,7 +38,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
@@ -46,14 +46,14 @@ class Users::RegistrationsController < Devise::RegistrationsController
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
-  # end
+  def configure_account_update_params
+    devise_parameter_sanitizer.permit(:account_update, keys: [:status])
+  end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+#   def after_sign_up_path_for(resource)
+#     super(resource)
+#   end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
